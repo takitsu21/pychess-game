78e81b3e2b87168fd63e4af6d37e83b1e802af72
Ralagane <42560304+Ralagane@users.noreply.github.com>
2018-08-30 00:41:31 +0200

    roi fait (en théorie)

--- echec.py
+++ echec.py
@@ -33,7 +33,7 @@ def affichePlateau():
             acc+=1
     print()
     print('    ',0,1,2,3,4,5,6,7)
-    print('   --------------->',' y')
+    print('     --------------->',' y')
 
 
 #Permet de vérifier si les coordonnées sont bien dans l'échiquier. Demande aussi à l'utilisateur de les saisir.
@@ -48,7 +48,13 @@ def verifCouleur(y,x):
     elif  plateau[y][x] == ".":
         return 'Vide'
     return 'Blanc'
-    
+
+#Permet de déplacer une pièce dans l'échiquier en partant du principe que tout a été vérifié
+def mouvementPiece(y1,x1,y2,x2):
+    tmp=plateau[y1][x1]
+    plateau[y1][x1]='.'
+    plateau[y2][x2]=tmp
+ 
 #Donne le type de la pièce à des coordonnées précis.
 def verifPiece(y,x):
     if plateau[y][x].lower()=='p':#pion
@@ -70,90 +76,175 @@ def pion(y1,x1,y2,x2):
     if verifCoord(y2,x2):
         if x2==x1:#Avancer tout droit
             if y1==6 and y2+2==y1 or y1==1 and y2-2==y1:#Avance de 2
-                tmp=plateau[y1][x1]
-                plateau[y1][x1]='.'
-                plateau[y2][x2]=tmp
+                mouvementPiece(y1,x1,y2,x2)
                 return 1
             elif y2+1==y1 or y2-1==y1 :#Avance de 1
-                tmp=plateau[y1][x1]
-                plateau[y1][x1]='.'
-                plateau[y2][x2]=tmp
+                mouvementPiece(y1,x1,y2,x2)
                 return 1
         elif verifPiece(y2,x2)!= 0 and verifCouleur(y1,x1)!=verifCouleur(y2,x2) and y2==y1-1 or y2==y1+1 and x2==x1+1 or x2==x1-1:#Manger piece
-            tmp=plateau[y1][x1]
-            plateau[y1][x1]='.'
-            plateau[y2][x2]=tmp
+            mouvementPiece(y1,x1,y2,x2)
             return 1
     return 0
           
 #Permet d'utiliser le fou.
-def fou(x1,y1,x2,y2):
+def fou(y1,x1,y2,x2):
     verifX=y1
     verifY=x1
-    if y1<0 or x1<0 or y2<0 or x2<0 or y1>8 or x1>8 or y2>8 or x2>8:
-        return 'impossible'
-    if y1>y2 and verifPiece(verifX-1,verifY-1)==0 or verifPiece(verifX+1,verifY-1)==0:#Monter
-        while verifX != y2 and verifY != x2:
-            if x1>x2 and verifPiece(verifX-1,verifY-1)==0:#monter gauche
-                verifX-=1
-                verifY-=1
-                print('haut gauche')
-            elif x1<x2 and verifPiece(verifX-1,verifY+1)==0 :#monter droite
-                verifX-=1
-                verifY+=1
-                print('haut droite')
-            elif verifPiece(verifX,verifY)!=0 :
-                return 'Impossible'
-    if y1<y2:#Descendre
-        while verifX != y2 and verifY != x2:
-            if y2>y1 and verifPiece(verifX+1,verifY+1)==0:#descendre droite
-                verifX+=1
-                verifY+=1
-                print('descend droite')
-            elif y2<y1 and verifPiece(verifX-1,verifY+1)==0:#descendre gauche
-                verifX-=1
-                verifY+=1
-                print('descend gauche')
-            elif verifPiece(verifX,verifY)!=0:          
-                return 'Impossible'
-    if verifPiece(y2,x2) == 0:
-        tmp=plateau[y1][x1]
-        plateau[y1][x1]='.'
-        plateau[y2][x2]=tmp
-    else:
-        return 'impossible'
-    affichePlateau()
+    if verifCoord(y2,x2):
+        if verifPiece(y2,x2)!= 0 and verifCouleur(y1,x1)!=verifCouleur(y2,x2):#Manger
+            if y1>y2:
+                while True:
+                    if x1>x2 and verifPiece(verifX-1,verifY-1)==0:#monter gauche
+                        verifX-=1
+                        verifY-=1
+                        if verifPiece(verifX-1,verifY-1)==0 :
+                            if verifPiece(verifX-2,verifY-2)!=0:
+                                mouvementPiece(y1,x1,y2,x2)
+                                return 1
+                            mouvementPiece(y1,x1,y2,x2)
+                            return 1   
+                    elif x1<x2 and verifPiece(verifX-1,verifY+1)==0 :#monter droite
+                        verifX-=1
+                        verifY+=1
+                        if verifPiece(verifX-1,verifY+1)==0:
+                            if verifPiece(verifX-2,verifY+2)!=0:
+                                mouvementPiece(y1,x1,y2,x2)
+                                return 1
+                            mouvementPiece(y1,x1,y2,x2)
+                            return 1                      
+            if y1<y2:#Descendre
+                while True:
+                    if y2>y1 and verifPiece(verifX+1,verifY+1)==0:#descendre droite
+                        verifX+=1
+                        verifY+=1
+                        if verifPiece(verifX+1,verifY+1)==0:
+                            if verifPiece(verifX+2,verifY+2)!=0:
+                                mouvementPiece(y1,x1,y2,x2)
+                                return 1
+                            mouvementPiece(y1,x1,y2,x2)
+                            return 1                       
+                    elif y2<y1 and verifPiece(verifX-1,verifY+1)==0:#descendre gauche
+                        verifX-=1
+                        verifY+=1
+                        if verifPiece(verifX-1,verifY+1)==0:
+                            if verifPiece(verifX-2,verifY+2)!=0:
+                                mouvementPiece(y1,x1,y2,x2)
+                                return 1
+                            mouvementPiece(y1,x1,y2,x2)
+                            return 1                    
+        else:
+            if y1>y2 and verifPiece(verifX-1,verifY-1)==0 or verifPiece(verifX+1,verifY-1)==0:#Monter
+                while verifX != y2 and verifY != x2:
+                    if x1>x2 and verifPiece(verifX-1,verifY-1)==0:#monter gauche
+                        verifX-=1
+                        verifY-=1
+                    elif x1<x2 and verifPiece(verifX-1,verifY+1)==0 :#monter droite
+                        verifX-=1
+                        verifY+=1
+                    elif verifPiece(verifX,verifY)!=0 :
+                        return 0
+                mouvementPiece(y1,x1,y2,x2)
+                return 1
+            if y1<y2:#Descendre
+                while verifX != y2 and verifY != x2:
+                    if y2>y1 and verifPiece(verifX+1,verifY+1)==0:#descendre droite
+                        verifX+=1
+                        verifY+=1
+                    elif y2<y1 and verifPiece(verifX-1,verifY+1)==0:#descendre gauche
+                        verifX-=1
+                        verifY+=1
+                    elif verifPiece(verifX,verifY)!=0:          
+                        return 0
+                mouvementPiece(y1,x1,y2,x2)
+                return 1
+    return 0
+
 
 def reine():
     return "slt g des boobs"
+
+#permet de voir si le roi à le champ libre pour se déplacer
+def verificationDeplacementValideDuRoi(y1,x1,y2,x2):
+    couleurDuRoi = verifCouleur(y1,x1)
+    for i in range((7-y2)+2): #vérification à droite du roi
+        if verifCouleur(y1+i,x1) == couleurDuRoi and y1+i !=y1: #Une pièce protège le roi
+            break
+        if verifPiece(y1+i,x1) =="q" and verifCouleur(y1+i,x1) != couleurDuRoi : 
+            print("Le roi ne peut pas se déplacer, car la reine restraint ses déplacements.")
+            return 0
+        if verifPiece(y1+i,x1) =="t" and verifCouleur(y1+i,x1) != couleurDuRoi:
+            print("Le roi ne peut pas se déplacer, car la tour restraint ses déplacements.")
+            return 0
+        
+    for i in range (y1+1): #vérification à gauche du roi
+         if verifCouleur(y1-i,x1) == couleurDuRoi and y1-i !=y1: #Une pièce protège le roi
+            break
+         if verifPiece(y1-i,x1) =="q" and verifCouleur(y1-i,x1) != couleurDuRoi: 
+             print("Le roi ne peut pas se déplacer, car la reine restraint ses déplacements.")
+             return 0
+         if verifPiece(y1-i,x1) =="t" and verifCouleur(y1-i,x1) != couleurDuRoi:
+            print("Le roi ne peut pas se déplacer, car la tour restraint ses déplacements.")
+            return 0
+        
+    for i in range ((7-y1)+1): #vérification en diagonale droit
+        if verifCouleur(y1+i,x1+i) == couleurDuRoi and y1+i !=y1 and x1+i!=x1: #Une pièce protège le roi
+            break
+        if verifPiece(y1+i,x1+i) =="q" and verifCouleur(y1+i,x1+i) != couleurDuRoi : 
+            print("Le roi ne peut pas se déplacer, car la reine restraint ses déplacements.")
+            return 0
+        if verifPiece(y1+i,x1+i) =="t" and verifCouleur(y1+i,x1+i) != couleurDuRoi:
+            print("Le roi ne peut pas se déplacer, car la tour restraint ses déplacements.")
+            return 0
+        
+    for i in range(y1+1): #vérification en diagonale gauche
+        if verifCouleur(y1-i,x1-i) == couleurDuRoi and y1-i != y1 and x1-i !=x1: #Une pièce protège le roi
+            break
+        if verifPiece(y1-i,x1-i) =="q" and verifCouleur(y1-i,x1-i) != couleurDuRoi : 
+            print("Le roi ne peut pas se déplacer, car la reine restraint ses déplacements.")
+            return 0
+        if verifPiece(y1-i,x1-i) =="t" and verifCouleur(y1-i,x1-i) != couleurDuRoi:
+            print("Le roi ne peut pas se déplacer, car la tour restraint ses déplacements.")
+            return 0
+        
+    for i in range (y1 +1) : #Vérification haut du roi
+        if verifCouleur(y1-i,x1) == couleurDuRoi and y1-i !=y1: #Une pièce protège le roi
+            break
+        if verifPiece(y1-i,x1) =="q" and verifCouleur(y1-i,x1) != couleurDuRoi : 
+            print("Le roi ne peut pas se déplacer, car la reine restraint ses déplacements.")
+            return 0
+        if verifPiece(y1-i,x1) =="t" and verifCouleur(y1-i,x1) != couleurDuRoi:
+            print("Le roi ne peut pas se déplacer, car la tour restraint ses déplacements.")
+            return 0
+    for i in range ((7-y1)+1): #Vérification bas du roi
+        if verifCouleur(y1+i,x1) == couleurDuRoi and y1+i !=y1: #Une pièce protège le roi
+            break
+        if verifPiece(y1+i,x1) =="q" and verifCouleur(y1+i,x1) != couleurDuRoi : 
+            print("Le roi ne peut pas se déplacer, car la reine restraint ses déplacements.")
+            return 0
+        if verifPiece(y1+i,x1) =="t" and verifCouleur(y1+i,x1) != couleurDuRoi:
+            print("Le roi ne peut pas se déplacer, car la tour restraint ses déplacements.")
+            return 0
+    return 1    
+
+# Permet de faire bouger le roi tout en regardant s'il peut bouger sans prolbème.
 def roi(y1,x1,y2,x2):
     if verifCouleur(y1,x1) ==verifCouleur(y2,x2)  :#On regarde ensuite si la pièce dans les coord d'arrivées est une alliée
-            print ("Vous tentez de capturer une pièce alliée, ce qui est impossible évidemment.")
-            return 0
-    if x1==x2 and y2 >y1 and y2-1 ==y1 : #déplacement de gauche à droite
-        for i in range((7-y2)+2): #vérification à droite du roi
-            if verifPiece(y1+i,x1) =="q" and verifCouleur(y1+i,x1) != verifCouleur(y1,x1): 
-                print("Le roi ne peut se déplacer, car la reine restraint ses déplacements.")
-                return 0
-            if verifPiece(y1+i,x1) =="t" and verifCouleur(y1+i,x1) != verifCouleur(y1,x1):
-                print("Le roi ne peut se déplacer, car la tour restraint ses déplacements.")
-                return 0
-        for i in range (y1+1): #vérification à gauche du roi
-             if verifPiece(y1-i,x1) =="q" and verifCouleur(y1+i,x1) != verifCouleur(y1,x1): 
-                 print("Le roi ne peut se déplacer, car la reine restraint ses déplacements.")
-                 return 0
-             if verifPiece(y1-i,x1) =="t" and verifCouleur(y1+i,x1) != verifCouleur(y1,x1):
-                print("Le roi ne peut se déplacer, car la tour restraint ses déplacements.")
-                return 0
-        for i in range ((7-y1)+1): #vérification en diagonale droit
-            if verifPiece(y1+i,x1+i) =='q' and verifCouleur(y1+i,x1+i) != verifCouleur(y1,x1) :
-                print("Le roi ne peut se déplacer en diagonale, car il y'a un fou un une reine qui lui barre le chemin.")
-                return 0
+        print ("Vous tentez de capturer une pièce alliée, ce qui est impossible évidemment.")
+        return 0
         
-            
-            
-                
-    
+    if x1==x2 and y2-1 ==y1 or y2+1 ==y1 : #déplacement honrizontal
+        if verificationDeplacementValideDuRoi(y1,x1,y2,x2) == 1:
+            mouvementPiece(y1,x1,y2,x2)
+            return 1
+        else:
+            return 0
+    if y1==y2 and x2-1 == x1 or x2+1 == x1: #déplacement vertical
+        if verificationDeplacementValideDuRoi(y1,x1,y2,x2) == 1:
+            mouvementPiece(y1,x1,y2,x2)
+            return 1
+        else:
+            return 0 
+
 def cavalier():
     return "slt c le cavalier"
 
@@ -168,36 +259,28 @@ def tour(y1,x1,y2,x2):
                if verifCouleur((y1),(x1+i)) !='Vide' and x1+i !=x1:
                    print ("La tour ne peut pas se déplacer car il y\'a une pièce sur son chemin.")
                    return 0
-               tmp=plateau[y1][x1]
-               plateau[y1][x1]='.'
-               plateau[y2][x2]=tmp
+               mouvementPiece(y1,x1,y2,x2)
                return 1
         if y1==y2 and verifCouleur(y1,x1) !=verifCouleur(y2,x2) and x2 <x1:#Déplacement de droite à gauche avec prise de pièce enemie
             for i in range ((x1-x2)+1):
                 if verifCouleur((y1),(x1-i)) !='Vide' and x1-i !=x1:
                     print ("La tour ne peut pas se déplacer car il y\'a une pièce sur son chemin.")
                     return 0
-            tmp=plateau[y1][x1]
-            plateau[y1][x1]='.'
-            plateau[y2][x2]=tmp
+            mouvementPiece(y1,x1,y2,x2)
             return 1
         if x1==x2 and verifCouleur(y1,x1) !=verifCouleur(y2,x2) and y2>y1:#Déplacement du haut vers le bas avec prise de pièce enemie
             for i in range ((y2-y1)+1):
                 if verifCouleur(y1+i,x1)!='Vide' and y1+i !=y1:
                     print ("La tour ne peut pas se déplacer car il y\'a une pièce sur son chemin.")
                     return 0
-            tmp=plateau[y1][x1]
-            plateau[y1][x1]='.'
-            plateau[y2][x2]=tmp
+            mouvementPiece(y1,x1,y2,x2)
             return 1
         if x1==x2 and verifCouleur(y1,x1) !=verifCouleur(y2,x2) and y1>y2:#Déplacement du bas vers le haut avec prise de pièce enemie
             for (i) in range ((y1-y2)+1):
                 if verifCouleur(y1-i,x1)!='Vide' and y1-i !=y1:
                     print ("La tour ne peut pas se déplacer car il y\'a une pièce sur son chemin.")
                     return 0
-            tmp=plateau[y1][x1]
-            plateau[y1][x1]='.'
-            plateau[y2][x2]=tmp
+            mouvementPiece(y1,x1,y2,x2)
             return 1
     else:
         print("Le coordonées données ne respectent pas les caractéristiques de la tour : la tour se déplace soit horizontalement ou verticalement.")
